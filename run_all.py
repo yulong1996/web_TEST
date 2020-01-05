5e45231ad46785ce8713ddf03862b7c90976021f
hyang621 <5384008+hyang621@user.noreply.gitee.com>
2020-01-05 19:41:55 +0800

    3 times

--- run_all.py
+++ run_all.py
@@ -3,7 +3,7 @@ from common import HTMLTestRunner_cn
 import time
 
 # 用例路径
-casePath = "F:\\web_auto_Test\\test_case"
+casePath = "F:\\jkjob\\workspace\\web_auto\\test_case"
 # 匹配规则
 rule = "test*.py"
 
@@ -12,7 +12,7 @@ discover = unittest.defaultTestLoader.discover(start_dir=casePath, pattern=rule)
 
 # 报告存放的路径
 now = time.strftime("%Y_%m_%d %H_%M_%S")
-reportPath = "F:\\web_auto_Test\\report\\" + now + "result.html"
+reportPath = "F:\\jkjob\\workspace\\web_auto\\report" + now + "result.html"
 # 打开文件写入
 fp = open(reportPath, "wb")
 
